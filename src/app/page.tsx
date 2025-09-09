b588a75ef85aa5500bda8b7e33a32cebec9d470d
DarkMatter <>
2025-09-09 14:38:43 +0000

    Add button to main page
    dm-checkpoint-id: 3fOcqO4MTtcV

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <div></div>
+    <div className="flex items-center justify-center min-h-screen">
+      <Button>Click me</Button>
+    </div>
   )
 }
